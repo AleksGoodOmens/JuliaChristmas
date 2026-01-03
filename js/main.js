a0376e1129dd8e28d2857056e04d61acc624f1e0
Aleks Gomeniuk <agsfinks@gmail.com>
2026-01-03 14:01:51 +0000

    feat: small changes from nikita

--- js/main.js
+++ js/main.js
@@ -5,32 +5,29 @@ import { hideOnScroll } from './hideOnScroll.js';
 import { renderSlides, Slider } from './slider.js';
 
 window.addEventListener('DOMContentLoaded', async () => {
-	const banner = document.querySelector('.banner');
-	const bannerText = document.querySelector('.banner__text span');
-	let isPaused = false;
+  const banner = document.querySelector('.banner');
+  const bannerText = document.querySelector('.banner__text span');
+  let isPaused = false;
 
-	banner.addEventListener('click', (e) => {
-		if (e.target.closest('.banner__icon')) {
-			bannerText.style.animationPlayState = 'running';
-		} else if (e.target.closest('.banner__text')) {
-			bannerText.style.animationPlayState = 'paused';
-		}
+  banner.addEventListener('mouseenter', (e) => {
+    if (!isPaused) bannerText.style.animationPlayState = 'paused';
+  });
 
-		isPaused = !isPaused;
-	});
+  banner.addEventListener('mouseleave', (e) => {
+    if (!isPaused) bannerText.style.animationPlayState = 'running';
+  });
+  new Countdown('.countdown', '2027-01-01T00:00:00').init();
 
-	new Countdown('.countdown', '2026-01-01T00:00:00').init();
+  const photos = await fetchData('./data/data.json');
+  if (photos) new Slider(`.promo__slider`, photos).init();
 
-	const photos = await fetchData('./data/data.json');
-	if (photos) new Slider(`.promo__slider`, photos).init();
+  const wishes = await fetchData('./data/wishes.json');
+  if (wishes) {
+    renderSlides('.swiper-wrapper', 'slider__slide swiper-slide', wishes);
+  }
+  createCityMarkers();
 
-	const wishes = await fetchData('./data/wishes.json');
-	if (wishes) {
-		renderSlides('.swiper-wrapper', 'slider__slide swiper-slide', wishes);
-	}
-	createCityMarkers();
+  actions.init('#dedmoroz', '.scale__bar', new Date(Date.UTC(2025, 0, 1, 0, 0, 0)));
 
-	actions.init('#dedmoroz', '.scale__bar', new Date(Date.UTC(2025, 0, 1, 0, 0, 0)));
-
-	hideOnScroll('.banner', 300, 500);
+  hideOnScroll('.banner', 300, 500);
 });
